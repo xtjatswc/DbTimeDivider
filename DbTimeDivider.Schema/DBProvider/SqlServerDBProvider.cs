55b4c59de96bbc0dbb7df2a930b2293d6aeb7d78
xtjatswc <565032163@qq.com>
2019-07-15 22:12:15 +0800

    抽象sqlserver provider

--- DbTimeDivider.Schema/DBProvider/SqlServerDBProvider.cs
+++ DbTimeDivider.Schema/DBProvider/SqlServerDBProvider.cs
@@ -3,20 +3,8 @@ using FluentData;
 
 namespace DbTimeDivider.Schema.DBProvider
 {
-    public class SqlServerDBProvider : AbsDBProvider
+    public class SqlServerDBProvider : SqlServerBaseProvider
     {
-        public override bool IsDatabaseExists(string dbName)
-        {
-            string sql = $"select count(*) from sys.databases where name = '{dbName}'";
-            return DbContext.Sql(sql).QuerySingle<int>() > 0;
-        }
-
-        public override bool IsTableExists(string tableName)
-        {
-            string sql = $"select count(*) from sysObjects where Id=OBJECT_ID(N'{tableName}') and xtype='U'";
-            return DbContext.Sql(sql).QuerySingle<int>() > 0;
-        }
-
         protected override string GetConnStr()
         {
             return string.Format(@"Server={0};Database={1};UID={2};Password={3};",
@@ -26,12 +14,5 @@ namespace DbTimeDivider.Schema.DBProvider
                 Database.Password
             );
         }
-
-        protected override IDbProvider GetDbProvider()
-        {
-            return new SqlServerProvider();
-        }
-
-
     }
 }
---
+++ DbTimeDivider/IFace/SqlServerBaseProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentData;
+
+namespace DbTimeDivider.IFace
+{
+    public abstract class SqlServerBaseProvider : AbsDBProvider
+    {
+        public override bool IsDatabaseExists(string dbName)
+        {
+            string sql = $"select count(*) from sys.databases where name = '{dbName}'";
+            return DbContext.Sql(sql).QuerySingle<int>() > 0;
+        }
+
+        public override bool IsTableExists(string tableName)
+        {
+            string sql = $"select count(*) from sysObjects where Id=OBJECT_ID(N'{tableName}') and xtype='U'";
+            return DbContext.Sql(sql).QuerySingle<int>() > 0;
+        }
+
+        protected override IDbProvider GetDbProvider()
+        {
+            return new SqlServerProvider();
+        }
+    }
+}
